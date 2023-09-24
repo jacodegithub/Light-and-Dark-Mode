6b17145a2660f367eab607ee912a6735d093f06c
jacodegithub <dudeobama246@gmail.com>
2023-09-24 16:13:58 +0530

    made improvement in not repeat yourself

--- script.js
+++ script.js
@@ -5,35 +5,46 @@ const textBox = document.getElementById('text-box')
 
 
 // DARKMODE FUNCTION
-function darkMode() {
+function darkAndLightMode(isDark) {
     // console.log(toggleIcon.children)
-    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)'
-    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)'
-    toggleIcon.children[0].textContent = 'Dark Mode'
-    toggleIcon.children[1].classList.remove('fa-sun')
-    toggleIcon.children[1].classList.add('fa-moon')
+    nav.style.backgroundColor = isDark ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)'
+    textBox.style.backgroundColor = isDark ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)'
+    toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode'
+    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon')
 }
 
 // LIGHTMODE FUNCTION 
-function lightMode() {
-    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)'
-    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)'
-    toggleIcon.children[0].textContent = 'Light Mode'
-    toggleIcon.children[1].classList.remove('fa-moon')
-    toggleIcon.children[1].classList.add('fa-sun')
-}
+// function darkAndLightMode(isLight) {
+//     nav.style.backgroundColor = 
+//     textBox.style.backgroundColor = 
+//     toggleIcon.children[0].textContent = 'Light Mode'
+//     toggleIcon.children[1].classList.remove('fa-moon')
+//     toggleIcon.children[1].classList.add('fa-sun')
+// }
 
 // SWITCHING THEME DYNAMICALLLY
 function switchTheme(event) {
     if(event.target.checked) {
+        localStorage.setItem('theme', 'dark')
         document.documentElement.setAttribute('data-theme', 'dark')
-        darkMode();
+        darkAndLightMode(true);
     }
     else {
         document.documentElement.setAttribute('data-theme', 'light')
-        lightMode();
+        darkAndLightMode(false);
     }
 }
 
 // EVENT LISTENER
 toggleSwitch.addEventListener('change', switchTheme)
+
+let currentTheme = localStorage.getItem('theme')
+
+if(currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme)
+
+    if(currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        darkAndLightMode();
+    }
+}
